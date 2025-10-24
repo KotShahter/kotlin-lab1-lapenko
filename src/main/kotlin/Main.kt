62bd0ba6bf10205f81ff407824b4d80f3101c586
IDTYZ <evlapenko9@gmail.com>
2025-10-24 22:33:41 +0300

    refactor: moved LogAnalyzer to a separate file

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,66 +1,19 @@
-import java.io.File
-
-class LogAnalyzer {
-
-    fun countByLevel(logList: List<LogEntry>): Map<String, Int> {
-        val groupedByFirstLetter = logList.groupingBy { it.level } //мамой клянусь, я офигеть как понимаю что это
-        val counts = groupedByFirstLetter.eachCount()
-        return counts;
-    }
-
-    fun findMostActiveService(logList: List<LogEntry>): String {
-        val result =
-            logList.maxByOrNull { it.service } // типа оно выдает первый максимальный элемент sqlной функции. все это оч похоже на бд
-        return result!!.service;
-    }
-
-    fun getErrorsForService(logList: List<LogEntry>, service: String): List<String> {
-        val groupedByFirstLetter = logList.groupBy { it.level }
-        return groupedByFirstLetter["ERROR"]!!.map { it.service }; //фп прикольное
-    }
-
-    fun analyzeMessageLengths(logList: List<LogEntry>): Any {
-        val lengths = logList.groupBy { it.level }.mapValues { it -> it.value.map { it.message.length }.average() }
-        return lengths
-    }
-
-    fun getTopNLongestMessages(logList: List<LogEntry>, TopN: Int): List<LogEntry> {
-        val groupedByFirstLetter = logList.sortedByDescending { it.toString().length }.take(TopN)
-        return groupedByFirstLetter.map { it }
-    }
-
-    fun filterByTimeRange(logList: List<LogEntry>, timestamp1: String, timestamp2: String): List<LogEntry> {
-        val groupedByFirstLetter = logList.filter { timestamp1 < it.timestamp && it.timestamp < timestamp2 }
-        return groupedByFirstLetter
-    }
-
-    fun getServiceLevelDistribution(logList: List<LogEntry>): Map<String, Map<String, Int>> {
-        val groupedByFirstLetter =
-            logList.groupBy { it.service }.mapValues { entry -> entry.value.groupingBy { it.level }.eachCount() }
-        return groupedByFirstLetter
-    }
-}
-
-// val parsedLogs = sampleLogs.mapNotNull {LogEntry(regex.matchEntire(it).destructured)}
-
-
 fun main() {
 
-    val analyzer = LogAnalyzer()
     val myJsonParser = LogParserFactory.createParser("json")
     val myTxtParser = LogParserFactory.createParser("text")
 
     val myLogs = myJsonParser.ParseLogs("examples\\exampleLogs.json")
     val myLogs2 = myTxtParser.ParseLogs("examples\\exampleLogs.json")
 
-    println(analyzer.countByLevel(myLogs))
-    println(analyzer.findMostActiveService(myLogs))
-    println(analyzer.getErrorsForService(myLogs, "123"))
+    println(LogAnalyzer.countByLevel(myLogs))
+    println(LogAnalyzer.findMostActiveService(myLogs))
+    println(LogAnalyzer.getErrorsForService(myLogs, "123"))
     //println(analyzeMessageLengths(MyLogs))
-    println(analyzer.getTopNLongestMessages(myLogs, 2))
+    println(LogAnalyzer.getTopNLongestMessages(myLogs, 2))
     println()
-    println(analyzer.analyzeMessageLengths(myLogs))
+    println(LogAnalyzer.analyzeMessageLengths(myLogs))
     println()
-    println(analyzer.filterByTimeRange(myLogs, "2024-01-15T10:28:17", "2024-01-15T10:32:12"))
+    println(LogAnalyzer.filterByTimeRange(myLogs, "2024-01-15T10:28:17", "2024-01-15T10:32:12"))
 }
 
